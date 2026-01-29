dbbff18b9b51fce36da130d88c42ecfaa92d00a4
Isaac Obo Enimil <isaac.enimil@amalitech.com>
2026-01-29 11:53:17 +0000

    Enhance Ansible and Terraform configurations for web server setup

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,16 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  # Professional Best Practice: S3 Backend
-  # Enable this block after creating an S3 bucket and DynamoDB table
-  # backend "s3" {
-  #   bucket         = "my-terraform-state-bucket"
-  #   key            = "ansible-terraform/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt        = true
-  # }
 }
 
 provider "aws" {
